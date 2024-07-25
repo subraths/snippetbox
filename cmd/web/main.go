deab8f9e1812cb5dc107c8c596cb1a20b3e02c3c
Subrath <subrathitachi6@gmail.com>
2024-07-25 17:32:08 +0530

    refactor(main.go): move main.go -> cmd dir
    move main.go to cmd dir and moved handlers from main.go to handlers.go

---
+++ cmd/web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Printf("Server running on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal((err))
+}
