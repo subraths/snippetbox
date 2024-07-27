38b6ee6a5409f8e146778d07925673c599095d61
Subrath <subrathitachi6@gmail.com>
2024-07-27 07:43:36 +0530

    feat(db): add pq and create db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq" // New import
 )
 
 type application struct {
@@ -14,10 +19,18 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network address")
+	dsn := flag.String("dsn", "postgres://mydb:secret@snippetbox-db-1/mydb?sslmode=disable", "Postgres data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +44,28 @@ func main() {
 	}
 
 	infoLog.Printf("Server running on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config
+	// struct.
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// Use PingContext() to establish a new connection to the database, passing in the
+	// context we created above as a parameter. If the connection couldn't be
+	// established successfully within the 5 second deadline, then this will return an
+	// error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// Return the sql.DB connection pool.
+	return db, nil
 }
